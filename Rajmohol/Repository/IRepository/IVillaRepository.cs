9489ab53930e5d78b21227930cd1e5e90d38f6c5
Abdul Aziz <Abdul Aziz@LAPTOP-B04RPVN4>
2024-07-03 23:59:42 +0600

    Create IVillaRepository and VillaRepository

---
+++ Rajmohol/Repository/IRepository/IVillaRepository.cs
@@ -0,0 +1,9 @@
+﻿using Rajmohol.Models;
+
+namespace Rajmohol.Repository.IRepository
+{
+    public interface IVillaRepository : IRepository<Villa>
+    {
+        Task<Villa> UpdateAsync(Villa entity);
+    }
+}
---
+++ Rajmohol/Repository/VillaRepository.cs
@@ -0,0 +1,23 @@
+﻿using Rajmohol.Data;
+using Rajmohol.Models;
+using Rajmohol.Repository.IRepository;
+
+namespace Rajmohol.Repository
+{
+    public class VillaRepository : Repository<Villa>, IVillaRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public VillaRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+            
+        }
+        public async Task<Villa> UpdateAsync(Villa entity)
+        {
+            entity.UpdatedDate = DateTime.Now;
+            _db.Update(entity);
+            await _db.SaveChangesAsync();
+            return entity;
+        }
+    }
+}
